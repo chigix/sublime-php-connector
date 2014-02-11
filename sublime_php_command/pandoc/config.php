73a6e30d571638c2b0f78602916471555409de0d
Richard Lea <chigix@zoho.com>
2014-02-12 01:47:55 +0800

    Update commands and pandoc configure file for html supporting.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- sublime_php_command/pandoc/config.php
+++ sublime_php_command/pandoc/config.php
@@ -1,3 +1,4 @@
 <?php
 	$config['pandoc']['template_path'] = 'D:/Developer/sublime_php_command/pages/pandoc/pm-template.latex';
+	$config['pandoc']['css_path'] = 'D:\Developer\sublime_php_command\pandoc\stylesheets\markdown10.css';
 ?>
