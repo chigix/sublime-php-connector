54ab3acad1b57cf2fc0ae36c196d7f2ee065e18a
Richard Lea <chigix@zoho.com>
2014-02-13 16:57:36 +0800

    UPDATE html template support.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- sublime_php_command/pandoc/config.php
+++ sublime_php_command/pandoc/config.php
@@ -1,4 +1,6 @@
 <?php
-	$config['pandoc']['template_path'] = 'D:/Developer/sublime_php_command/pages/pandoc/pm-template.latex';
-	$config['pandoc']['css_path'] = 'D:\Developer\sublime_php_command\pandoc\stylesheets\markdown10.css';
+	$config['pandoc']['template_pdf'] = 'D:/Developer/sublime_php_command/pandoc/template/pm-template.latex';
+	$config['pandoc']['template_html'] = 'D:/Developer/sublime_php_command/pandoc/template/pm-template.html5';
+	// $config['pandoc']['css_path'] = 'D:\Developer\sublime_php_command\pandoc\stylesheets\markdown10.css';
+	$config['pandoc']['css_path'] = 'D:\Developer\sublime_php_command\pandoc\stylesheets\pandoc.css';
 ?>
