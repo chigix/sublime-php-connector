cf767daebd6fbe175931b3215a0fe45dfbcd7bb8
Richard Lea <chigix@zoho.com>
2014-03-28 00:21:31 +0800

    ADD check_env trigger support when first boot.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- php_connector.py
+++ php_connector.py
@@ -1,10 +1,12 @@
 import sublime, sublime_plugin
-import os, subprocess, string, json, threading, re
+import os, subprocess, string, json, threading, re, time
 from chigi_args import ChigiArgs
+from check_env import CheckEnvironmentCommandThread
 
 class PhpConnectorCommand(sublime_plugin.WindowCommand):
 	def __init__(self, window):
 		self.window = window;
+		setting = sublime.load_settings("phpConnector.sublime-settings");
 	def run(self):
 		ListCommandThread(self.window).start()
 	def is_visible(self):
@@ -20,18 +22,17 @@ class ListCommandThread(threading.Thread):
 		self.chigiArgs = args.getArgs();
 		self.setting = sublime.load_settings("phpConnector.sublime-settings");
 		self.commandsDir = self.setting.get("commands_dir");
-		self.php_path = self.setting.get("php_path");
 		self.window = window;
 		self.currentFileName = self.chigiArgs.get('view').file_name();
 		threading.Thread.__init__(self);
 
     def run(self):
-        self.commandList = [];
-        self.commandObjList = [];
-        if self.chigiArgs.get('quick_load') is True:
-        	# Accelerate on ctrl+shift+r
-        	f = file(self.commandsDir+'/List.json');
-        	for command in json.load(f):
+		self.commandList = [];
+		self.commandObjList = [];
+		if self.chigiArgs.get('quick_load') is True:
+			# Accelerate on ctrl+shift+r
+			f = file(self.commandsDir+'/List.json');
+			for command in json.load(f):
 				for format in command.get('format'):
 					if self.currentFileName[-len(format):] == format:
 						tmp_to_list = [];
@@ -39,23 +40,23 @@ class ListCommandThread(threading.Thread):
 						self.commandObjList.append(command);
 						self.commandList.append(tmp_to_list);
 						pass;
-		pass;
-        else:
-        	# fetch the data of command list and render it into panel directly
-	        self.chigiArgs.get("view");
-        	for command in self.chigiArgs.get("commandList"):
-			    print(command.get('format'));
-			    tmp_to_list = [];
-			    tmp_to_list.append(command.get('name'));
-			    self.commandObjList.append(command);
-			    self.commandList.append(tmp_to_list);
-        	pass;
-        def show_quick_panel():
+			pass;
+		else:
+			# fetch the data of command list and render it into panel directly
+			self.chigiArgs.get("view");
+			for command in self.chigiArgs.get("commandList"):
+				print(command.get('format'));
+				tmp_to_list = [];
+				tmp_to_list.append(command.get('name'));
+				self.commandObjList.append(command);
+				self.commandList.append(tmp_to_list);
+			pass;
+		def show_quick_panel():
             # if not self.package_list:
                 # print('There are no packages to list')
                 # return
-            self.window.show_quick_panel(self.commandList, self.on_done)
-        sublime.set_timeout(show_quick_panel, 10)
+			self.window.show_quick_panel(self.commandList, self.on_done)
+		sublime.set_timeout(show_quick_panel, 10)
     def on_done(self, picked):
         """
         Quick panel user selection handler - opens the homepage for any
@@ -71,7 +72,7 @@ class ListCommandThread(threading.Thread):
 	        	{
 	        		"script":commandPicked['script'],
 	        		"classPath":commandPicked['class'],
-	        		"args_string":commandPicked['args_string']
+	        		"user_args":commandPicked['user_args']
 	        	});
         	pass;
         else:
