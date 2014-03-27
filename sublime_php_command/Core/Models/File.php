87b6ea061a2921e9f1ab3179c0b0d241f91769c1
Richard Lea <chigix@zoho.com>
2014-03-28 00:20:28 +0800

    FIX fileSystemEncoding effect in File.php
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- sublime_php_command/Core/Models/File.php
+++ sublime_php_command/Core/Models/File.php
@@ -1,44 +1,62 @@
-<?php 
-	namespace Chigi\Sublime\Models;
+<?php
 
-	use Chigi\Sublime\Settings\Environment;
-	use Chigi\Sublime\Models\FileNotFoundException;
-	
-	class File extends \SplFileInfo {
-		/**
-    		 * Constructs a new file from the given path.
-		 *
-     	 * @param string  $path      The path to the file
-     	 * @param Boolean $checkPath Whether to check the path or not
-     	 *
-     	 * @throws FileNotFoundException If the given path is not a file
-     	 *
-     	 * @api
-     	 */
-    	 	public function __construct($path, $checkPath = true)
-    		{
-        		if ($checkPath && !is_file($path)) {
-            		throw new FileNotFoundException($path);
-        		}
-        		parent::__construct($path);
-    		}
-		
-		public function getDirPath(){
-			return dirname($this->getRealPath());
-		}
-		
-		public function extractFileName(){
-			$file_name = substr($this->getRealPath(), 
-				$tempos = strripos($this->getRealPath(),'/')+1, 
-				strripos($this->getRealPath(), '.')-$tempos);
-			return $file_name;
-		}
-		public function getRealPath(){
-			return str_replace('\\', '/', parent::getRealPath());
-		}
-		public function extractFileSuffix(){
-			$suffix = substr($a_replaced, strripos($a_replaced, '.')+1);
-			return $suffix;
-		}
-	}
+namespace Chigi\Sublime\Models;
+
+use Chigi\Sublime\Settings\Environment;
+use Chigi\Sublime\Exception\FileNotFoundException;
+
+class File extends \SplFileInfo {
+
+    /**
+     * Constructs a new file from the given path.
+     *
+     * @param string  $path      The path to the file
+     * @param Boolean $checkPath Whether to check the path or not
+     * @param Boolean $fileSystemEncoding Specific the filesystem encoding
+     *
+     * @throws FileNotFoundException If the given path is not a file
+     *
+     * @api
+     */
+    public function __construct($path, $checkPath = true, $fileSystemEncoding = null) {
+        if (is_null($fileSystemEncoding)) {
+            $fileSystemEncoding = Environment::getInstance()->getFileSystemEncoding();
+        }
+        if ($checkPath && !is_file(iconv('utf-8', $fileSystemEncoding, $path))) {
+            throw new FileNotFoundException($path);
+        }
+        parent::__construct(iconv('utf-8', $fileSystemEncoding, $path));
+    }
+
+    /**
+     * 获取绝对路径
+     * @param boolean $rawSysEncode 是否采用FileSystemEncoding，针对PHP内部字符串处理则使用false
+     * @return string
+     */
+    public function getRealPath($rawSysEncode = true) {
+        return $rawSysEncode ?
+                (parent::getRealPath()) :
+                (iconv(Environment::getInstance()->getFileSystemEncoding(), 'utf-8', parent::getRealPath()));
+    }
+
+    /**
+     * 获取当前文件所在目录路径
+     * @return string
+     */
+    public function getDirPath() {
+        return dirname($this->getRealPath(FALSE));
+    }
+
+    public function extractFileName() {
+        $file_name = substr($this->getRealPath(false), $tempos = strripos($this->getRealPath(false), DIRECTORY_SEPARATOR) + 1, strripos($this->getRealPath(false), '.') - $tempos);
+        return $file_name;
+    }
+
+    public function extractFileSuffix() {
+        $suffix = substr($this->getRealPath(false), strripos($this->getRealPath(false), '.') + 1);
+        return $suffix;
+    }
+
+}
+
 ?>
