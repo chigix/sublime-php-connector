9fc461af3850cb079a2e202deb9495538e458902
Richard Lea <chigix@zoho.com>
2014-04-22 20:29:15 +0800

    ADD autoload support for different fileSystemEncoding
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- sublime_php_command/Core/autoload.php
+++ sublime_php_command/Core/autoload.php
@@ -18,7 +18,6 @@ function __autoload($class) {
         $quickPath = array(
             '${entryDir}' => $baseDir
         );
-        //$map = require_once('autoload_namespaces.php');
         $map = Environment::getInstance()->getNamespacesMap();
         for ($i = count($parts) - 1; $i >= -1; $i--) {
             $tmpNamespace = implode('\\', $parts);
@@ -28,7 +27,11 @@ function __autoload($class) {
                                 array_keys($quickPath), array_values($quickPath), $map[$tmpNamespace]
                         )
                         . '/' . implode('/', $tmpParts) . '.php';
-                if (!@include_once(iconv('utf-8', Environment::getInstance()->getFileSystemEncoding(), $scriptFile))) {
+                if (@include_once(iconv('utf-8', Environment::getInstance()->getFileSystemEncoding(), $scriptFile))){ // @ - to suppress warnings, 
+                    ;
+                } else if(@include_once($scriptFile)){
+                    ;
+                } else{
                     throw new ScriptNotFoundException($scriptFile);
                 }
                 return;
@@ -38,16 +41,30 @@ function __autoload($class) {
                                 array_keys($quickPath), array_values($quickPath), $map[$tmpNamespace . '\\']
                         )
                         . '/' . implode('/', $tmpParts) . '.php';
-                if (!@include_once(iconv('utf-8', Environment::getInstance()->getFileSystemEncoding(), $scriptFile))) // @ - to suppress warnings, 
+                if (@include_once(iconv('utf-8', Environment::getInstance()->getFileSystemEncoding(), $scriptFile))){ // @ - to suppress warnings, 
+                    ;
+                } else if(@include_once($scriptFile)){
+                    ;
+                } else{
                     throw new ScriptNotFoundException($scriptFile);
+                }
                 return;
-            }
-            // 都不符合，则去掉末节namespace，重新检索
-            try {
-                array_unshift($tmpParts, $parts[$i]);
-                unset($parts[$i]);
-            } catch (Exception $e) {
-                return;
+            }else{
+                // 都不符合，则将末节 namespace 移至 $tmpParts 前缀拼接，重新检索
+                try {
+                    array_unshift($tmpParts, $parts[$i]);
+                    unset($parts[$i]);
+                } catch (\Chigi\Sublime\Exception\ArrayIndexOutOfBoundsException $e) {
+                    $callStack = debug_backtrace();
+                    $lastStack = $callStack[0];
+                    $exToThrow = new \Chigi\Sublime\Exception\ClassNotFoundException($class,0,null);
+                    $exToThrow->setFile($lastStack['file'])->setLine($lastStack['line']);
+                    throw $exToThrow;
+                    return;
+                } catch (Exception $e) {
+                    throw $e;
+                    return;
+                }
             }
         }
     }
