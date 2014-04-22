8027b3d84468fd834669e3554aae37701c320f4b
Richard Lea <chigix@zoho.com>
2014-04-22 21:24:50 +0800

    ADD dirname function support from different fileSystem encoding.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- sublime_php_command/Commands/Chigi/pandoc/config.php
+++ sublime_php_command/Commands/Chigi/pandoc/config.php
@@ -1,7 +1,8 @@
 <?php
-$config['pandoc']['template_pdf'] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . 'pm-template.latex';
-$config['pandoc']['template_html'] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . 'pm-template.html5';
+use Chigi\Sublime\Settings\Environment;
+$config['pandoc']['template_pdf'] = iconv(Environment::getInstance()->getFileSystemEncoding(), 'utf-8', dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . 'pm-template.latex';
+$config['pandoc']['template_html'] = iconv(Environment::getInstance()->getFileSystemEncoding(), 'utf-8', dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . 'pm-template.html5';
 // $config['pandoc']['css_path'] = 'D:\Developer\sublime_php_command\pandoc\stylesheets\markdown10.css';
-$config['pandoc']['css_path'] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'stylesheets' . DIRECTORY_SEPARATOR . 'pandoc.css';
-$config['pandoc']['exe'] = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'pandoc.exe';
+$config['pandoc']['css_path'] = iconv(Environment::getInstance()->getFileSystemEncoding(), 'utf-8', dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'stylesheets' . DIRECTORY_SEPARATOR . 'pandoc.css';
+$config['pandoc']['exe'] = iconv(Environment::getInstance()->getFileSystemEncoding(), 'utf-8', dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'bin' . DIRECTORY_SEPARATOR . 'pandoc.exe';
 ?>
