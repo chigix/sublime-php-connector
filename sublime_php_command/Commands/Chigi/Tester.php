09151cdf45606911a306366c0f2816d6ea3c9262
Richard Lea <chigix@zoho.com>
2014-06-22 23:55:45 +0800

    Prettify the code format.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- sublime_php_command/Commands/Chigi/Tester.php
+++ sublime_php_command/Commands/Chigi/Tester.php
@@ -1,17 +1,23 @@
 <?php
-	namespace Chigi;
-	use Chigi\Sublime\Models\ReturnData;
-	use Chigi\Sublime\Settings\Environment;
-	class Tester{
-		public function run(){
-			$env = Environment::getInstance();
-			$data = json_decode(file_get_contents(dirname($env->getCorePath()) . '/List.json'));
-			$returnData = new ReturnData();
-			$returnData->setCode(239);
-			$returnData->setMsg('Show Command list on quick panel.');
-			$returnData->setStatusMsg(NULL);
-			$returnData->setData($data);
-			return $returnData;
-		}
-	}
+
+namespace Chigi;
+
+use Chigi\Sublime\Models\ReturnData;
+use Chigi\Sublime\Settings\Environment;
+
+class Tester {
+
+    public function run() {
+        $env = Environment::getInstance();
+        $data = json_decode(file_get_contents(dirname($env->getCorePath()) . '/List.json'));
+        $returnData = new ReturnData();
+        $returnData->setCode(239);
+        $returnData->setMsg('Show Command list on quick panel.');
+        $returnData->setStatusMsg(NULL);
+        $returnData->setData($data);
+        return $returnData;
+    }
+
+}
+
 ?>
