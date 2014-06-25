128e393d6d593995235478f411991decb62ee909
Richard Lea <chigix@zoho.com>
2014-06-25 20:53:44 +0800

    Separate executePush data processor into an individual function, executeReceive. ADD QUICK_PANEL DATA support.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- php_output.py
+++ php_output.py
@@ -21,6 +21,56 @@ class PhpOutputThread(threading.Thread):
         self.stdout = stdout;
     def __del__(self):
         os.system("php -a");
+        
+    def executeReceive(self, result):
+        returned_data = result[2];
+        data_type = "UNKNONW";
+        if(result[0][1] is 0):
+            data_type = "NUMBER";
+        elif result[0][1] is 1:
+            data_type = "STRING";
+        elif result[0][1] is 2:
+            data_type = "EXCEPTION";
+        elif result[0][1] is 3:
+            data_type = "ARRAY";
+        elif result[0][1] is 4:
+            data_type = "OBJECT";
+        elif result[0][1] is 5:
+            data_type = "NONE";
+        if(result[0][2] is 1):
+            # TODO Open a file
+            pass;
+        elif result[0][2] is 3:
+            # RUN Developed PHP CMD OBJ
+            self.runCmd("ax_text",{
+                "call" : result[2][2], 
+                "cmd_args":result[2][7]
+            });
+            pass;
+        elif result[0][2] is 6:
+            print(u"【INFO】 " + result[1]);
+        elif result[0][2] is 7:
+            # TODO Copy to clipboard
+            pass;
+        elif result[0][2] is 8:
+            # DEBUG
+            print(u"【" + data_type + u"】 " + result[1]);
+            print(returned_data);
+        elif result[0][2] is 9:
+            # TODO show quick panel
+            show_list = [];
+            obj_list = [];
+            for item in result[2]:
+                show_list.append(item[0]);
+                obj_list.append(item[1]);
+            def quick_panel_ondone(input):
+                self.executeReceive(obj_list[input]);
+            def quick_panel_run_command():
+                ChigiArgs.GetInstance().currentView.window().show_quick_panel(show_list, quick_panel_ondone);
+            sublime.set_timeout(quick_panel_run_command, 1)
+            pass;
+        else:
+            print(result);
 
     def run(self):
         temp_buffer = '';
@@ -56,38 +106,12 @@ class PhpOutputThread(threading.Thread):
                     # -------------------------------------------------------------------
                     #                 PHP 通信完成，开始处理结果
                     # -------------------------------------------------------------------
-                    returned_data = result[2];
-                    data_type = "UNKNONW";
-                    if(result[0][1] is 0):
-                        data_type = "NUMBER";
-                    elif result[0][1] is 1:
-                        data_type = "STRING";
-                    elif result[0][1] is 2:
-                        data_type = "EXCEPTION";
-                    elif result[0][1] is 3:
-                        data_type = "ARRAY";
-                    elif result[0][1] is 4:
-                        data_type = "OBJECT";
-                    elif result[0][1] is 5:
-                        data_type = "NONE";
-                    if(result[0][2] is 1):
-                        # TODO Open a file
-                        pass;
-                    elif result[0][2] is 6:
-                        print(u"【INFO】 " + result[1]);
-                    elif result[0][2] is 7:
-                        # TODO Copy to clipboard
-                        pass;
-                    elif result[0][2] is 8:
-                        # DEBUG
-                        print(u"【" + data_type + u"】 " + result[1]);
-                        print(returned_data);
-                    elif result[0][2] is 9:
-                        # TODO show quick panel
-                        pass;
-                    else:
-                        print(result);
+                    self.executeReceive(result);
                 else:
                     temp_buffer = temp_buffer + out;
     def onDone(self, input):
         pass;
+    def runCmd(self, name, args):
+        def now_to_run():
+            ChigiArgs.GetInstance().currentView.window().run_command(name, args);
+        sublime.set_timeout(now_to_run, 1);
